a393153c3ef3d5c097c5ea2572c1421b6b797e84
gizzemcann <gzmcan1616@gmail.com>
2025-04-19 16:53:42 +0300

    Create 66.Plus One.py

---
+++ 66.Plus One.py
@@ -0,0 +1,16 @@
+def plusOne_algo(digits):
+    """
+    digits: List[int] — Her eleman bir basamaktır.
+    return: List[int] — Sayıya 1 eklenmiş hali.
+    """
+    n = len(digits)
+    
+    # Sondan başa doğru git
+    for i in range(n - 1, -1, -1):
+        if digits[i] < 9:
+            digits[i] += 1  # Direkt artır
+            return digits
+        digits[i] = 0  # 9 ise 0 yap, elde var 1
+
+    # Hepsi 9'duysa başa 1 ekle
+    return [1] + [0] * n
